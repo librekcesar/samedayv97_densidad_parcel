6c5bcd17b08e1a697e2efd5c2a9c2fd64e3934b9
librekcesar <140665209+librekcesar@users.noreply.github.com>
2026-01-29 23:54:49 -0600

    Add files via upload

---
+++ puntos_bodegas_copia.js
@@ -0,0 +1,10 @@
+var centro_ideal = {
+"type": "FeatureCollection",
+"name": "puntos_bodegas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "nombre": null }, "geometry": { "type": "Point", "coordinates": [ -99.18234773985786, 19.431410410124005 ] } },
+{ "type": "Feature", "properties": { "id": 2, "nombre": null }, "geometry": { "type": "Point", "coordinates": [ -99.15827850686712, 19.429608318414779 ] } },
+{ "type": "Feature", "properties": { "id": 3, "nombre": null }, "geometry": { "type": "Point", "coordinates": [ -99.141410344795162, 19.640744074453256 ] } }
+]
+}
